826aa002e43a81ba0e3225c4847397528e7efea1
Rendell Lugtu <pmnp4a.me@gmail.com>
2026-03-24 22:20:23 +0800

    removal of about

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -84,13 +84,6 @@ export default function Navbar() {
                   >
                     Requirements
                   </Link>
-                  <Link
-                    to="/about"
-                    onClick={() => setDropdown(null)}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    About
-                  </Link>
                 </div>
               )}
             </div>
